e67cb16224d6665406dc5d4533cfc96f5926110b
Evgen Kotlyarchuck <demomusicevcat@gmail.com>
2023-05-07 22:14:34 +0300

    add docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3002,
+        port: 3000,
+        host: true,
+        strictPort: true,
+    },
+    preview: {
+        port: 8080,
     },
 });
